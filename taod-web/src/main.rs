ecc2b5fd75b316d6751848b5d37e47e1789a07a9
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-11-07 16:28:41 +0900

    Implement a feature to run http server.

--- taod-web/src/main.rs
+++ taod-web/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    HttpServer::new(move || App::new().service(health_check))
+        .bind(("127.0.0.1", 8002))?
+        .run()
+        .await
+        .map_err(|e| anyhow::anyhow!(e))
+}
+
+#[get("/health-check")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
 }
